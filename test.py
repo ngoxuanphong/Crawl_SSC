1678b328acbf9a27bd93631ab078835af899c83c
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2022-06-26 04:13:21 +0700

    fix bug

--- test.py
+++ test.py
@@ -14,18 +14,16 @@ for folder in list_folder:
     if (os.path.exists(folder) == False) or (os.path.isdir(folder) == False):
         os.mkdir(folder)
 
-symbol = 'A32'
+symbol = '24H'
 year = '2021'
 all_com = pd.read_excel('base/data/List_Com_First (1_4).xlsx')['Symbol']
 all_com = list(all_com)
-# for i in range(0, len(all_com)):
-#     symbol = all_com[i]
-#     print(i, symbol)
-#     for finan in ['BS']:
-#         for year in ['2018', '2019', '2020', '2021']:
-#             if finan == 'BS':
-#                 web.login(symbol, year, finan).to_csv(f'{ps_y_BS}{year}{symbol}.csv', index = False)
-#             if finan == 'IS':
-#                 web.login(symbol, year, finan).to_csv(f'{ps_y_IS}{year}{symbol}.csv', index = False)
-finan = 'IS'
-web.login(symbol, year, finan).to_csv(f'{ps_y}{year}_{symbol}_{finan}.csv', index = False)
+for i in range(0, len(all_com)):
+    symbol = all_com[i]
+    print(i, symbol, end='__:')
+    for finan in ['BS', 'IS']:
+        for year in ['2018', '2019', '2020', '2021']:
+            web.login(symbol, year, finan).to_csv(f'{ps_y}{symbol}_{year}_{finan}.csv', index = False)
+    print('Done')
+
+# finan = 'IS'
