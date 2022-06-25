7031a0e9fc25f663d3f0d7253c76d7fac8621254
DESKTOP-25QH34T\ADMIN <xuanphongcht@gmail.com>
2022-06-26 04:10:40 +0700

    fix bug

--- base/getlink.py
+++ base/getlink.py
@@ -29,6 +29,8 @@ class setup():
 class get(setup):
     def login(self, symbol, year, finan):
         self.driver.get(self.link)
+        # self.getlink(self.link)
+        time.sleep(0.5)
         self.driver.maximize_window()
         check_element = self.sendelenmet(symbol, year, finan)
         if check_element == 'Loi':
@@ -38,31 +40,55 @@ class get(setup):
         data = self.read_table()
         return data
 
+    def getlink(self, link):
+        try:
+            self.driver.set_page_load_timeout(10)
+            self.driver.get(link)
+        except:
+            print('hi')
+            # self.driver.refresh()
+            self.getlink(link)
+
     def sendelenmet(self, symbol, year, finan):
         self.driver.find_element_by_id('pt9:it8112::content').send_keys(symbol)
+        time.sleep(0.5)
         self.driver.find_element_by_id('pt9:it8112::content').send_keys(Keys.ENTER)
         self.driver.find_element_by_id('pt9:smc2::content').click()
+        time.sleep(0.5)
         self.driver.find_element_by_xpath('//*[@id="pt9:smc2::pop"]/li[2]/ul/li[11]/label/input').click()
+        time.sleep(0.5)
         self.driver.find_element_by_xpath('//*[@id="pt9:smc2::pop"]/li[2]/ul/li[11]/label/input').send_keys(Keys.ENTER)
         self.driver.find_element_by_id('pt9:b1').click()
-        time.sleep(3)
+        time.sleep(7)
         id = self.ChooseLink_table(year)
         print(id)
         if id == None:
             return 'Loi'
         self.driver.find_element_by_id(id).click()
+        time.sleep(1)
         if finan == 'IS':
             self.driver.find_element_by_id('pt2:tab2::disAcr').click()
         
-    def ChooseLink_table(self, year):
+    def Choose_id(self, year):
         driver_page_source = self.driver.page_source
         page = BeautifulSoup(driver_page_source, "html.parser")
         check = page.find('table', {'role':'presentation',"class":"x14q x15f"})
         span_page = check.find_all('a', {'class':'xgl'})
         for i in span_page:
-            if 'hợp nhất' in i.text and year in i.text:
+            if 'năm' in i.text and year in i.text and 'Riêng' not in i.text:  
                 id = i['id']
                 return id
+    def ChooseLink_table(self, year):
+        id = self.Choose_id(year)
+        if id != None:
+            return id
+        else:
+            try:
+                self.driver.find_element_by_id('pt9:t1::nb_nx').click()
+                time.sleep(2)
+                return self.Choose_id(year)
+            except: pass
+        
 
     def read_table(self):
         driver_page_source = self.driver.page_source
